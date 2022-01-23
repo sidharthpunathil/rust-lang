d62c3978b2d1e997fb4d27cb659fb7a88eacfb6d
Sidharth P <sidharthpunathil714@gmail.com>
2022-01-23 15:17:57 +0530

    chapter 4 done

---
+++ 4. Understanding Ownership/ownerships/target/debug/deps/ownerships-3b5a528e7f69470a.d
@@ -0,0 +1,5 @@
+/development/rust-lang/4. Understanding Ownership/ownerships/target/debug/deps/ownerships-3b5a528e7f69470a: src/main.rs
+
+/development/rust-lang/4. Understanding Ownership/ownerships/target/debug/deps/ownerships-3b5a528e7f69470a.d: src/main.rs
+
+src/main.rs:
---
+++ 4. Understanding Ownership/ownerships/target/debug/ownerships.d
@@ -0,0 +1,1 @@
+/development/rust-lang/4.\ Understanding\ Ownership/ownerships/target/debug/ownerships: /development/rust-lang/4.\ Understanding\ Ownership/ownerships/src/main.rs
