b7dcb53ac4fe53cbc0501109b24aa8cf8e32feca
Sidharth P <sidharthpunathil714@gmail.com>
2022-01-28 01:25:13 +0530

    chapter 8 done

---
+++ 8. Common Collections/hashmap/src/main.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    // println!("{:?}", score);
+    match score {
+        Some(i) => println!("The score is: {}", i),
+        _ => println!("{:?}", score),
+    }
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> =
+        teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    println!("{:?}", scores);
+}
---
+++ 8. Common Collections/vectors/src/main.rs
@@ -0,0 +1,25 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
+
